57977b3ca8fbfa05bfb7f0c63489d834c526afb8
segundevs <tushbash@gmail.com>
2022-04-22 07:38:53 -0700

    added new component

---
+++ src/components/CurrentSavings.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Avatar from 'react-avatar';
+
+const CurrentSavings = ({ currentSavings }) => {
+  return (
+    <div className='current_savings'>
+      <Avatar color="rgba(0, 48, 73, 1)" name="John Doe" size="60" round={true} className="current_savings-avatar"/>
+      <div className="current_savings-amount">
+        <p>Current Savings: </p>
+        <h3>₦ {currentSavings.toLocaleString()}</h3>
+      </div>
+      
+    </div>
+  )
+}
+
+export default CurrentSavings
