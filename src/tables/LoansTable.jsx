3380400cfd7f967f837616c21e6564f22bff50dd
segundevs <tushbash@gmail.com>
2022-04-28 01:30:26 -0700

    added loans table

---
+++ src/tables/LoansTable.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const LoansTable = ({col1, col2, col3, col4, col5, data, setClickId}) => {
+
+  return (
+    <div className="table_container">
+      <table>
+        <thead>
+          <tr className="table_header">
+            <th>{col1}</th>
+            <th>{col2}</th>
+            <th>{col3}</th>
+            <th>{col4}</th>
+            <th>{col5}</th>
+          </tr>
+        </thead>
+        {data?.map(row => (
+          <tbody key={row.id}>
+            <tr onClick={() => setClickId(row.id)} >
+              <td>{row.category}</td>
+              <td>₦ {row?.amount.toLocaleString()}</td>
+              <td>{row.description}</td>
+              <td>{row.percentage}%</td>
+              <td>{row.date}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+    );
+};
+
+export default LoansTable;
