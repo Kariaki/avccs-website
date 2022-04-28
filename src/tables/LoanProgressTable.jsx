bfd3010c63b20676c49adcc080d95188ffc8502c
segundevs <tushbash@gmail.com>
2022-04-28 01:30:04 -0700

    added loan progress table

---
+++ src/tables/LoanProgressTable.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const LoanProgressTable = ({col1, col2, col3, data}) => {
+   return (
+    <div className="loan_progress-table-container">
+      <table>
+        <thead>
+          <tr className="table_header">
+            <th>{col1}</th>
+            <th>{col2}</th>
+            <th>{col3}</th>
+          </tr>
+        </thead>
+        {data?.map(row => (
+          <tbody key={row.id}>
+            <tr>
+              <td>₦ {row.amount.toLocaleString()}</td>
+              <td>{`${row.description}`.charAt(0).toUpperCase() + `${row.description}`.slice(1)}</td>
+              <td>{row.date}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+    );
+}
+
+export default LoanProgressTable
