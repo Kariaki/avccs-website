dd74323d2fc636827a3d08f0eaf0673a292d8a3d
segundevs <tushbash@gmail.com>
2022-04-28 01:38:15 -0700

    added savings table

---
+++ src/tables/SavingsTable.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md';
+
+const SavingsTable = ({col1, col2, col3, col4, data}) => {
+
+  return (
+    <div className="table_container">
+      <table>
+        <thead>
+          <tr className="table_header">
+            <th>{col1}</th>
+            <th>{col2}</th>
+            <th>{col3}</th>
+            <th>{col4}</th>
+          </tr>
+        </thead>
+        {data?.map(row => (
+          <tbody key={row.id}>
+            <tr>
+              <td>₦ {row.amount.toLocaleString()}</td>
+              <td>{`${row.description}`.charAt(0).toUpperCase() + `${row.description}`.slice(1)}</td>
+              <td>{row.amount >= 0 ? <MdArrowDropUp size={18} color='green'/> : <MdArrowDropDown size={18} color='red'/>}</td>
+              <td>{row.date}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+    );
+};
+
+export default SavingsTable;
