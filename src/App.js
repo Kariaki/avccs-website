2b484aa5402a8fbdfa0bd2243984d48d25dee082
segundevs <tushbash@gmail.com>
2022-05-10 14:08:04 -0700

    updated project styles

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import Ledger from "./pages/Ledger";
 
 //Components
 import PrivateRoute from "./utils/PrivateRoute";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/ledger" element={<Ledger />} />
-        </Route>
-      </Routes>
-    </div>
+    <React.Fragment>
+      <div className="mobile">Please view on a desktop device</div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/ledger" element={<Ledger />} />
+          </Route>
+        </Routes>
+      </div>
+    </React.Fragment>
   );
 }
 
